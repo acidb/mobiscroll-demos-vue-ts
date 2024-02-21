9bb0e118ee51ac2476320186db33b736fadc467e
Halmen Istvan <isti@mobiscroll.com>
2024-02-21 09:28:02 +0200

    Add isMbscDemo flag

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+;(window as Window & { isMbscDemo?: boolean }).isMbscDemo = true
+
 const app = createApp(App)
 
 app.use(router)
